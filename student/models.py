efb316da7f76048434b54e327ea34bb56196fe5e
sameera638 <shaiksammu638@gmail.com>
2025-05-09 16:15:42 +0530

    first commit

--- student/models.py
+++ student/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db import models
 class Student(models.Model):
     name=models.CharField(max_length=20)
-    roll_no=models.IntegerField(max_length=5)
+    roll_no=models.IntegerField(max_length=3)
     mobile=models.CharField(max_length=10)
 
     
